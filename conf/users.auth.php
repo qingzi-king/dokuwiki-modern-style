41670d511302af580807e718269acdaba6b63eac
王轻姿 <qingzi.wang@cq-tct.com>
2016-03-07 10:09:53 +0800

    initial commit

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$eXpfOv63$OWqXqtfJQgu9TPXHbLYku1:超级管理员:qingzi.wang@cq-tct.com:admin,user
