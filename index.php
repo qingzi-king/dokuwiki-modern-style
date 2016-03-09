ecdcb5f01506c2b40599ad7d463b8a538f6d297b
王轻姿 <qingzi.wang@cq-tct.com>
2016-03-09 10:09:29 +0800

    fix: 对于bootstrap模板修改细节样式

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
-<?php
-/**
- * Forwarder to doku.php
- *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Andreas Gohr <andi@splitbrain.org>
- */
-header("Location: doku.php");
+<?php 
+//满足条件跳转
+
+$userInfo = [
+  // "dingdingUserId"=>"02375134001386",
+  "name"=>"王轻姿",
+  "email"=>"qingzi.wang@cq-tct.com"
+];
+
+echo $userInfo[dingdingUserId];
+
+if($userInfo[dingdingUserId]) {
+  echo "<script>";
+  echo "location.href = 'http://172.16.103.9:8888/wiki/dokuwiki-modern-style/doku.php?id=start';";
+  echo "</script>";
+}else {
+  echo "哈哈哈";
+}
+
+
+
+?>
+
+
+<script>  
+  // location.href = "http://172.16.103.9:8888/wiki/dokuwiki-modern-style/doku.php?id=start";
+</script>
+
+
+
